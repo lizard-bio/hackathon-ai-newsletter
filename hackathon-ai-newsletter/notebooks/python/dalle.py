7a42179e558f0a71b5e5d986bef33169aee74584
Alexander Koch <info@alexanderkoch.be>
2023-09-14 14:16:44 +0200

    Different prompt building strategy

--- hackathon-ai-newsletter/notebooks/python/dalle.py
+++ hackathon-ai-newsletter/notebooks/python/dalle.py
@@ -36,16 +36,18 @@ def build_dalle_prompt(text):
 			{'role': 'user', 'content': f"""
 				Below is the summary of a scientific publication, enclosed by triple backticks.
 				Based on this summary, create a prompt for DALL-E so that it can generate a beautiful and 
-				captivating image to visually represent the summary. This prompt cannot be longer than 50 words.
+				captivating image to visually represent the summary. This prompt cannot be longer than 30 words.
 				It also has to be simple, descriptive, and specific.
-				When the prompt is ready, attach the words "captivating", "scientific illustration", "nature", 
-				"publication-ready", "science", "visual".
 
 				```{text}```
 				"""}
 		]
 	)
-	return response['choices'][0]['message']['content']
+	gpt_prompt = response['choices'][0]['message']['content']
+	gpt_prompt += ", scientific illustration in the style of Charles Darwin"
+	gpt_prompt += ", nature"
+	gpt_prompt += ", science"
+	return gpt_prompt
 
 
 def generate_image(prompt):
